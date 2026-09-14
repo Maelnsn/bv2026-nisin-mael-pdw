7cb3c47691593ae048cc77f44d2310cfd901bf42
Maelnsn <maelnisin@hotmail.be>
2026-09-14 17:41:18 +0200

    bravo ia

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@root/app.module.js';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { EnvService } from './common/config/env.service.js';
 
-export const bootstrap = async () : Promise<void> => {
+export const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
+const app = await NestFactory.create<NestExpressApplication>(
+  AppModule.register(),
+);
+
+const envService = app.get(EnvService);
+await app.listen(envService.appPort);
 
 void bootstrap();
--- api/src/root/app.module.ts
+++ api/src/root/app.module.ts
@@ -1,7 +1,21 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { HealthModule } from '@core/health/health.module.js';
+import { EnvService } from 'src/common/config/env.service.js';
+import { AppConfigModule } from 'src/common/config/app-config.module.js';
 
-@Module({
-  imports: [HealthModule]
-})
-export class AppModule {}
+@Module({})
+export class AppModule {
+  static register(): DynamicModule {
+    return {
+      module: AppModule,
+      imports: [AppConfigModule.register(), HealthModule],
+    };
+  }
+}
+
+const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+const envService = app.get(EnvService);
+await app.listen(envService.appPort);
