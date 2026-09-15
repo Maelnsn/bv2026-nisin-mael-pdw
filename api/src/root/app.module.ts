cf04522fa813c455c2768e1c7375af88f2317c7a
Maelnsn <maelnisin@hotmail.be>
2026-09-15 17:43:58 +0200

    health import

--- api/src/root/app.module.ts
+++ api/src/root/app.module.ts
@@ -2,13 +2,14 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from '@common/config/app-config.module';
+import { HealthModule } from '@core/health';
 
 @Module({})
 export class AppModule {
   static register(): DynamicModule {
     return {
       module: AppModule,
-      imports: [AppConfigModule.register()],
+      imports: [AppConfigModule.register(), HealthModule],
       controllers: [AppController],
       providers: [AppService],
     };
