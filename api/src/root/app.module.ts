590ee731b9097bb32d27fa63d8d096959338bb2e
Maelnsn <maelnisin@hotmail.be>
2026-09-21 14:59:06 +0200

    LOGS DONE AND READY

--- api/src/root/app.module.ts
+++ api/src/root/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from '@common/config/app-config.module';
 import { HealthModule } from '@core/health';
+import { LoggingModule } from '@common/logging/logging-module';
 
 @Module({})
 export class AppModule {
   static register(): DynamicModule {
     return {
       module: AppModule,
-      imports: [AppConfigModule.register(), HealthModule],
+      imports: [AppConfigModule.register(), HealthModule, LoggingModule],
       controllers: [AppController],
       providers: [AppService],
     };
