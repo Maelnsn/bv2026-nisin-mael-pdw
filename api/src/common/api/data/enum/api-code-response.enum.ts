20b7442de5138497d81c74adc91ccd5f43a5291b
Maelnsn <maelnisin@hotmail.be>
2026-09-24 12:37:41 +0200

    je déteste le cours

---
+++ api/src/common/api/data/enum/api-code-response.enum.ts
@@ -0,0 +1,12 @@
+export enum ApiCodeResponse {
+  CommonSuccess = 'api.common.success',
+  CommonError = 'api.common.error',
+  CommonValidationError = 'api.common.validation-error',
+  CommonInvalidIdentifier = 'api.common.invalid-identifier',
+  AuthRegistrationSuccess = 'api.auth.register.success',
+  AuthInvalidCredentials = 'api.auth.invalid-credentials',
+  AuthEmailAlreadyUsed = 'api.auth.email-already-used',
+  AuthUnauthorized = 'api.auth.unauthorized',
+  AuthCsrfInvalid = 'api.security.csrf-invalid',
+  AccountMeSuccess = 'api.account.me.success',
+}
